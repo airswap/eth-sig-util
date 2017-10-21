9924b81849c55d453f32abb8da91fec50e1c1466
cloudonshore <samuel.walker9@gmail.com>
2017-10-20 21:26:56 -0400

    added 2015 preset

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-const ethUtil = require('ethereumjs-util');
-const ethAbi = require('ethereumjs-abi');
+'use strict';
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var ethUtil = require('ethereumjs-util');
+var ethAbi = require('ethereumjs-abi');
 
 module.exports = {
 
-  concatSig: function (v, r, s) {
-    const rSig = ethUtil.fromSigned(r);
-    const sSig = ethUtil.fromSigned(s);
-    const vSig = ethUtil.bufferToInt(v);
-    const rStr = padWithZeroes(ethUtil.toUnsigned(rSig).toString('hex'), 64);
-    const sStr = padWithZeroes(ethUtil.toUnsigned(sSig).toString('hex'), 64);
-    const vStr = ethUtil.stripHexPrefix(ethUtil.intToHex(vSig));
+  concatSig: function concatSig(v, r, s) {
+    var rSig = ethUtil.fromSigned(r);
+    var sSig = ethUtil.fromSigned(s);
+    var vSig = ethUtil.bufferToInt(v);
+    var rStr = padWithZeroes(ethUtil.toUnsigned(rSig).toString('hex'), 64);
+    var sStr = padWithZeroes(ethUtil.toUnsigned(sSig).toString('hex'), 64);
+    var vStr = ethUtil.stripHexPrefix(ethUtil.intToHex(vSig));
     return ethUtil.addHexPrefix(rStr.concat(sStr, vStr)).toString('hex');
   },
 
-  normalize: function (input) {
+  normalize: function normalize(input) {
     if (!input) return;
 
     if (typeof input === 'number') {
-      const buffer = ethUtil.toBuffer(input);
+      var buffer = ethUtil.toBuffer(input);
       input = ethUtil.bufferToHex(buffer);
     }
 
     if (typeof input !== 'string') {
-      let msg = 'eth-sig-util.normalize() requires hex string or integer input.';
-      msg += ` received ${typeof input}: ${input}`;
+      var msg = 'eth-sig-util.normalize() requires hex string or integer input.';
+      msg += ' received ' + (typeof input === 'undefined' ? 'undefined' : _typeof(input)) + ': ' + input;
       throw new Error(msg);
     }
 
     return ethUtil.addHexPrefix(input.toLowerCase());
   },
 
-  personalSign: function (privateKey, msgParams) {
+  personalSign: function personalSign(privateKey, msgParams) {
     var message = ethUtil.toBuffer(msgParams.data);
     var msgHash = ethUtil.hashPersonalMessage(message);
     var sig = ethUtil.ecsign(msgHash, privateKey);
@@ -38,33 +42,33 @@ module.exports = {
     return serialized;
   },
 
-  recoverPersonalSignature: function (msgParams) {
-    const publicKey = getPublicKeyFor(msgParams);
-    const sender = ethUtil.publicToAddress(publicKey);
+  recoverPersonalSignature: function recoverPersonalSignature(msgParams) {
+    var publicKey = getPublicKeyFor(msgParams);
+    var sender = ethUtil.publicToAddress(publicKey);
     senderHex = ethUtil.bufferToHex(sender);
     return senderHex;
   },
 
-  extractPublicKey: function (msgParams) {
-    const publicKey = getPublicKeyFor(msgParams);
+  extractPublicKey: function extractPublicKey(msgParams) {
+    var publicKey = getPublicKeyFor(msgParams);
     return '0x' + publicKey.toString('hex');
   },
 
-  typedSignatureHash: function (typedData) {
-    const hashBuffer = typedSignatureHash(typedData);
+  typedSignatureHash: function typedSignatureHash(typedData) {
+    var hashBuffer = _typedSignatureHash(typedData);
     return ethUtil.bufferToHex(hashBuffer);
   },
 
-  signTypedData: function (privateKey, msgParams) {
-    const msgHash = typedSignatureHash(msgParams.data);
-    const sig = ethUtil.ecsign(msgHash, privateKey);
+  signTypedData: function signTypedData(privateKey, msgParams) {
+    var msgHash = _typedSignatureHash(msgParams.data);
+    var sig = ethUtil.ecsign(msgHash, privateKey);
     return ethUtil.bufferToHex(this.concatSig(sig.v, sig.r, sig.s));
   },
 
-  recoverTypedSignature: function (msgParams) {
-    const msgHash = typedSignatureHash(msgParams.data);
-    const publicKey = recoverPublicKey(msgHash, msgParams.sig);
-    const sender = ethUtil.publicToAddress(publicKey);
+  recoverTypedSignature: function recoverTypedSignature(msgParams) {
+    var msgHash = _typedSignatureHash(msgParams.data);
+    var publicKey = recoverPublicKey(msgHash, msgParams.sig);
+    var sender = ethUtil.publicToAddress(publicKey);
     return ethUtil.bufferToHex(sender);
   }
 
@@ -72,33 +76,33 @@ module.exports = {
    * @param typedData - Array of data along with types, as per EIP712.
    * @returns Buffer
    */
-};function typedSignatureHash(typedData) {
-  const error = new Error('Expect argument to be non-empty array');
-  if (typeof typedData !== 'object' || !typedData.length) throw error;
+};function _typedSignatureHash(typedData) {
+  var error = new Error('Expect argument to be non-empty array');
+  if ((typeof typedData === 'undefined' ? 'undefined' : _typeof(typedData)) !== 'object' || !typedData.length) throw error;
 
-  const data = typedData.map(function (e) {
+  var data = typedData.map(function (e) {
     return e.value;
   });
-  const types = typedData.map(function (e) {
+  var types = typedData.map(function (e) {
     return e.type;
   });
-  const schema = typedData.map(function (e) {
+  var schema = typedData.map(function (e) {
     if (!e.name) throw error;
-    return `${e.type} ${e.name}`;
+    return e.type + ' ' + e.name;
   });
 
   return ethAbi.soliditySHA3(['bytes32', 'bytes32'], [ethAbi.soliditySHA3(new Array(typedData.length).fill('string'), schema), ethAbi.soliditySHA3(types, data)]);
 }
 
 function recoverPublicKey(hash, sig) {
-  const signature = ethUtil.toBuffer(sig);
-  const sigParams = ethUtil.fromRpcSig(signature);
+  var signature = ethUtil.toBuffer(sig);
+  var sigParams = ethUtil.fromRpcSig(signature);
   return ethUtil.ecrecover(hash, sigParams.v, sigParams.r, sigParams.s);
 }
 
 function getPublicKeyFor(msgParams) {
-  const message = ethUtil.toBuffer(msgParams.data);
-  const msgHash = ethUtil.hashPersonalMessage(message);
+  var message = ethUtil.toBuffer(msgParams.data);
+  var msgHash = ethUtil.hashPersonalMessage(message);
   return recoverPublicKey(msgHash, msgParams.sig);
 }
 
